9407f5144bbb9ad5bb2b806ef750678466dd7450
LENOVO <LENOVO@DESKTOP-TJ4NT7M>
2024-02-15 17:47:58 +0530

    Base change

---
+++ src/practice/com/Forlooppractice.java
@@ -0,0 +1,12 @@
+package practice.com;
+
+public class Forlooppractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String name="Cricket is mty religion sachin is my god";
+	System.out.println(name.indexOf("sachin"));
+
+	}
+
+}
